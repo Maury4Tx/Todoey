134cccea1dd64df3ab6779d6215a173b508a8b87
William Harris <wharris@aol.com>
2018-02-19 20:17:22 -0600

    MVC with Ternary Operator

---
+++ Todoey/DataModel/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by William Harris on 2/19/18.
+//  Copyright © 2018 100defapps. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title: String = ""
+    var done: Bool = false
+    
+}
